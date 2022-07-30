b23a90064384a7f65e086336f51eb0e8bd2232ca
rouamansour <rouamansour.rm@gmail.com>
2022-07-30 21:34:25 +0100

    first commit

---
+++ sidebar/src/sections/@dashboard/requests/index.js
@@ -0,0 +1,3 @@
+export { default as DemandeListHead } from './RequestListHead';
+export { default as DemandeListToolbar } from './RequestListToolbar';
+export { default as DemandeMoreMenu } from './RequestMoreMenu';
